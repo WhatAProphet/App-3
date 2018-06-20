6c63d2066ecc630102b75957fa20ddba033ea6e5
t-constantin <dcelevol63@gmail.com>
2018-06-20 07:00:35 -0400

    me

--- non_connecte/client/SocketClientNonConnecte.c
+++ non_connecte/client/SocketClientNonConnecte.c
@@ -34,7 +34,7 @@ main(argc, argv)  int argc; char *argv[];
 	 }
      
  
-     // ?????????????????????????????????????????????
+     // Création d'un socket utilisant le protocole UDP
      SendingSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	 if (SendingSocket < 0)
 	 {
@@ -42,61 +42,52 @@ main(argc, argv)  int argc; char *argv[];
 		 exit(1);
 	 }
 
-	 // ?????????????????????????????????????????????
+	 // Définition de la structure du socket récepteur
      ReceiverAddr.sin_family = AF_INET;
 	 printf("Entrer le nom de la machine distante: ");
 	 gets(buf);
-	 hp = gethostbyname(buf);      /*?????????????????????*/
+	 hp = gethostbyname(buf);      //Établie le nom de l'appareil récepteur
 	 if (hp == 0)
 	 {
 		 fprintf(stderr, "%s: machine inconnue\n", buf);
 		 exit(1);
 	 }
-	 memcpy((char *)&ReceiverAddr.sin_addr, (char *)hp->h_addr, hp->h_length);  /*?????????????????????*/
+	 memcpy((char *)&ReceiverAddr.sin_addr, (char *)hp->h_addr, hp->h_length);  //Copie l'adresse du récepteur 
 	 printf("Entrer le numero de port dans la machine distante: ");
 	 gets(buf);
-	 ReceiverAddr.sin_port = htons((short)atoi(buf)); /*?????????????????????*/
+	 ReceiverAddr.sin_port = htons((short)atoi(buf)); //Établie le numéro du port avec le socket récepteur de l'aute appareil
 
+	 //Début de l'envoie du fichier
 	 FILE *file;
 	 while (1)
 	 {
 		 printf("\nVeuillez entrer le nom du fichier: ");
 		 scanf("%s", fileName);
-		 file = fopen(fileName, "rb");
-		 if (file != NULL)
+		 file = fopen(fileName, "rb"); //Ouverture du fichier en mode lecture bytes
+		 if (file != NULL) //Vérifie que le fichier s'est bien ouvert'
 		 {
 			 TotalByteSent = sendto(SendingSocket, fileName, sizeof(fileName), 0,
-				 (struct sockaddr *)&ReceiverAddr, sizeof(ReceiverAddr));
+				 (struct sockaddr *)&ReceiverAddr, sizeof(ReceiverAddr)); //Envoie du nom du fichier au récepteur
 			 printf("\nLe contenu du fichier sera imprime");
-			 while (!feof(file))
+			 while (!feof(file)) //Lecture du fichier
 			 {
 				 fread(SendBuf, BUFFER_LENGTH, 1, file);
 				 TotalByteSent = sendto(SendingSocket, SendBuf, sizeof(SendBuf), 0,
-					 (struct sockaddr *)&ReceiverAddr, sizeof(ReceiverAddr));
+					 (struct sockaddr *)&ReceiverAddr, sizeof(ReceiverAddr)); //Envoie d'une partie des données au récepteur
 				 printf("Nombre de bytes envoyes: %d\n", TotalByteSent);
 				 Sleep(3);
 			 } 
-			 printf("\nFinal size: %d\n", totalBytesInFile);
 			 fclose(file);
 			 break;
 		 }
-		 else
+		 else//S'il y a eu un problème à l'ouverture du fichier
 		 {
 			 printf("\nLe nom de fichier est incorrect.");
 		 }
 		 
 	 }
-	 closesocket(SendingSocket);
-	 
- 
-	 // ?????????????????????????????????????????????
-     /*printf("Client: les donnees a envoyer : \"%s\"\n", SendBuf);
-     printf("Client: entrain d'envoyer...\n");
-	 TotalByteSent = sendto(SendingSocket, SendBuf, BUFFER_LENGTH, 0,
-		 (struct sockaddr *)&ReceiverAddr, sizeof(ReceiverAddr));*/
 
- 
-	 // ?????????????????????????????????????????????
+	 
      memset(&SrcInfo, 0, sizeof(SrcInfo));
      len = sizeof(SrcInfo);
  
@@ -104,15 +95,16 @@ main(argc, argv)  int argc; char *argv[];
      printf("Client: adresse  IP d'envoi: %s\n", inet_ntoa(SrcInfo.sin_addr));
      printf("Client: port utilise: %d\n", htons(SrcInfo.sin_port));
  
-	 // ?????????????????????????????????????????????
+	 // Imprime et reçoie les information du socket récepteur
 	 getpeername(SendingSocket, (struct sockaddr *)&ReceiverAddr, (int *)sizeof(ReceiverAddr));
      printf("Client: adresse IP du serveur: %s\n", inet_ntoa(ReceiverAddr.sin_addr));
      printf("Client: port du serveur: %d\n", htons(ReceiverAddr.sin_port));
  
-	 // ?????????????????????????????????????????????
-	
+	 // Fermeture du socket
+	 closesocket(SendingSocket);
+
 	 
-	// ?????????????????????????????????????????????
+	// Terminaison de Winsock
 	WSACleanup();
 	// Back to the system
 	return 0;
--- non_connecte/serveur/SocketServeurNonConnecte.c
+++ non_connecte/serveur/SocketServeurNonConnecte.c
@@ -33,93 +33,93 @@ char *argv[];
 	 }
 	 WORD version = (1 << 8) + 1;  /* Version 1.1 de WINSOCK*/
 
-	 // ????????????????
+	 // Initialisation de Winsock
 	 if (WSAStartup(version, &wsaData) != 0)
 	 {
 		 perror("lors de l'initialisation winsock");
 		 exit(1);
 	 }
  
-	 // ????????????????
+	 // On vérifie si l'ouverture du socket est un succès
 	 if ((ReceivingSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
 	 {
 		 perror("lors de l'ouverture du socket");
 		 exit(1);
 	 }
 	 printf("Socket cree\n");
-	 // ????????????????
+	 // Définition du type d'adresse : IPv4
      ReceiverAddr.sin_family = AF_INET;
-	 // ????????????????
+	 // Assigne l'adresse IP de la machine utilisée pour le socket du serveur
 	 ReceiverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	 if (argc == 2)  /*?????????????????????*/
-		 ReceiverAddr.sin_port = htons((short)atoi(argv[1]));  /*?????????????????????*/
+	 if (argc == 2)  
+		 ReceiverAddr.sin_port = htons((short)atoi(argv[1]));  //Assigne le port disponible
 	 else
-		 ReceiverAddr.sin_port= 0;  /*?????????????????????*/
+		 ReceiverAddr.sin_port= 0;  //On n'assigne pas de port
     
  
-	 // ????????????????
+	 // Liaison du socket à l'adresse IPv4
 	 if (bind(ReceivingSocket, (struct sockaddr  *)&ReceiverAddr, ReceiverAddrSize) <0)
 		{ 
 			perror("lors de l'association au numero de port");
 			exit(1);
 		}
 	
-	 // ????????????????
-	 if (getsockname(ReceivingSocket, (struct sockaddr *)&ReceiverAddr, &ReceiverAddrSize)< 0) /*?????????????????????*/
+	 // On assure la validité du socket dans le serveur
+	 if (getsockname(ReceivingSocket, (struct sockaddr *)&ReceiverAddr, &ReceiverAddrSize)< 0) //On assure d'avoir une réponse du socket
 		{
 			perror("lors de l'interrogation sur le socket");
 			exit(1);
 		}
 
-	 // ????????????????
+	 // Impression à l'écran des informations obtenues précédemment
 
    printf("Serveur: adresse IP utilisee: %s\n", inet_ntoa(ReceiverAddr.sin_addr));
    printf("Serveur: port utilise %d\n", htons(ReceiverAddr.sin_port));
    printf("Server: pret a recevoir...\n");
  
-   // ????????????????
+   // Préparation à la réception d'un fichier
 
    FILE *fptr;
-   int sendbuflen = BUFFER_LENGTH;
-   char *sendbuf[BUFFER_LENGTH];
    ByteReceived = recvfrom(ReceivingSocket, ReceiveBuf, sizeof(ReceiveBuf), 0,
-	   (struct sockaddr *)&SenderAddr, &SenderAddrSize);
-   fptr = fopen(ReceiveBuf, "wb");
+	   (struct sockaddr *)&SenderAddr, &SenderAddrSize); //Les premiers bytes reçues seront le nom du fichier qui sera transferré
+   fptr = fopen(ReceiveBuf, "wb"); //Ouverture du fichier en mode: écriture bytes
    printf("Fichier cree au nom de: %s\n", ReceiveBuf);
+
+   //Réception des données qui seront envoyées
    while (ByteReceived > 0)
    {
 	   ByteReceived = recvfrom(ReceivingSocket, ReceiveBuf, sizeof(ReceiveBuf), 0,
-		   (struct sockaddr *)&SenderAddr, &SenderAddrSize);
+		   (struct sockaddr *)&SenderAddr, &SenderAddrSize); //Réception des bytes
 	   byteReceivedCount += ByteReceived;
-	   if (ByteReceived > 0)
+	   if (ByteReceived > 0) //Lorsque des bytes auront été reçus
 	   {
 		   printf("Serveur: Nombre de bytes recus: %d\n", ByteReceived);
 		   fwrite(ReceiveBuf, BUFFER_LENGTH, 1, fptr);
 		   Sleep(3);
 		   //sendto(ReceivingSocket, (const char *)answer, strlen(answer), 0, (struct sockaddr *)&SenderAddr, SenderAddrSize);
 	   }
-	   else if (ByteReceived == 0)
+	   else if (ByteReceived == 0) //Lorsqu'il n'y a plus de bytes à recevoir
 	   {
 		   printf("Connection done");
 	   }
-	   else
+	   else //En cas d'erreur lors de la réception
 		   printf("Serveur: recvfrom() non reussi avec code: %d\n", WSAGetLastError());
    }
    
    fclose(fptr);
    
  
- // ????????????????
+ // Reçoie et imprime les informations du client
  getpeername(ReceivingSocket, (SOCKADDR *)&SenderAddr, &SenderAddrSize);
  printf("Serveur: adresse IP du client: %s\n", inet_ntoa(SenderAddr.sin_addr));
  printf("Server: numero de port du client: %d\n", htons(SenderAddr.sin_port));
  printf("Server: Bytes received: %d\n", byteReceivedCount);
 
- // ????????????????
+ // Fermeture du socket
  printf("Serveur: Fini de recevoir...\n");
  closesocket(ReceivingSocket);
 
- // ????????????????
+ // Met fin à l'utilisation de Winsock
 
 WSACleanup(); 
 return 0;
